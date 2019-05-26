5c145f50accf8a5cc7039b2f88e9d4f805672758
Lucas Gomes <lucasgsm88@gmail.com>
2019-05-26 01:09:24 -0300

    adicionado propriedades as classes

--- backend/Entidades/Entidades/ItemAchado.cs
+++ backend/Entidades/Entidades/ItemAchado.cs
@@ -1,7 +1,17 @@
-﻿namespace Entidades.Entidades
+﻿using System.Collections.Generic;
+
+namespace Entidades.Entidades
 {
     public class ItemAchado : Item
     {
         public bool Devolvido { get; set; }
+        public long ItemPerdidoMatchId { get; set; }
+        public virtual ItemPerdido ItemPerdidoMatch { get; set; }
+        public virtual List<ItemPerdido> ItensPerdidosMatch { get; set; }
+
+        public ItemAchado()
+        {
+            ItensPerdidosMatch = new List<ItemPerdido>();
+        }
     }
 }
--- backend/Entidades/Entidades/ItemPerdido.cs
+++ backend/Entidades/Entidades/ItemPerdido.cs
@@ -1,7 +1,18 @@
-﻿namespace Entidades.Entidades
+﻿using System.Collections.Generic;
+
+namespace Entidades.Entidades
 {
     public class ItemPerdido : Item
     {
+
         public bool Recuperado { get; set; }
+        public long ItemAchadoMatchId { get; set; }
+        public virtual ItemAchado ItemAchadoMatch { get; set; }
+        public virtual List<ItemAchado> ItensAchadosMatch { get; set; }
+
+        public ItemPerdido()
+        {
+            ItensAchadosMatch = new List<ItemAchado>();
+        }
     }
 }
